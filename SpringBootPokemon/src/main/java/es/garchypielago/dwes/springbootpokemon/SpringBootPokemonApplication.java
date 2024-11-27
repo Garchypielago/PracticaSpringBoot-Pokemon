79d81e918134669267e276aef288194a29e02f25
garchy-home <jandro.garcia.sanchez@gmail.com>
2024-11-27 18:52:31 +0100

    Inicio de repositorio
    Inicio

---
+++ SpringBootPokemon/src/main/java/es/garchypielago/dwes/springbootpokemon/SpringBootPokemonApplication.java
@@ -0,0 +1,13 @@
+package es.garchypielago.dwes.springbootpokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPokemonApplication.class, args);
+    }
+
+}
---
+++ SpringBootPokemon/src/test/java/es/garchypielago/dwes/springbootpokemon/SpringBootPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package es.garchypielago.dwes.springbootpokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
