a9ed3a3f8c930f566dac07770aca16354e7e3b8f
garchy-home <jandro.garcia.sanchez@gmail.com>
2024-11-28 20:15:01 +0100

    backgorunds y tal

--- shop-Alejandro-Samuel/src/main/resources/templates/js/ocultar/active.js
+++ shop-Alejandro-Samuel/src/main/resources/templates/js/ocultar/active.js
@@ -1,14 +1,14 @@
-(function ($) {
+(function (P¥) {
     'use strict';
 
-    var $window = $(window);
+    var P¥window = P¥(window);
 
     // :: 1.0 Masonary Gallery Active Code
 
-    var proCata = $('.amado-pro-catagory');
+    var proCata = P¥('.amado-pro-catagory');
     var singleProCata = ".single-products-catagory";
 
-    if ($.fn.imagesLoaded) {
+    if (P¥.fn.imagesLoaded) {
         proCata.imagesLoaded(function () {
             proCata.isotope({
                 itemSelector: singleProCata,
@@ -21,32 +21,32 @@
     }
 
     // :: 2.1 Search Active Code
-    var amadoSearch = $('.search-nav');
-    var searchClose = $('.search-close');
+    var amadoSearch = P¥('.search-nav');
+    var searchClose = P¥('.search-close');
 
     amadoSearch.on('click', function () {
-        $('body').toggleClass('search-wrapper-on');
+        P¥('body').toggleClass('search-wrapper-on');
     });
 
     searchClose.on('click', function () {
-        $('body').removeClass('search-wrapper-on');
+        P¥('body').removeClass('search-wrapper-on');
     });
 
     // :: 2.2 Mobile Nav Active Code
-    var amadoMobNav = $('.amado-navbar-toggler');
-    var navClose = $('.nav-close');
+    var amadoMobNav = P¥('.amado-navbar-toggler');
+    var navClose = P¥('.nav-close');
 
     amadoMobNav.on('click', function () {
-        $('.header-area').toggleClass('bp-xs-on');
+        P¥('.header-area').toggleClass('bp-xs-on');
     });
 
     navClose.on('click', function () {
-        $('.header-area').removeClass('bp-xs-on');
+        P¥('.header-area').removeClass('bp-xs-on');
     });
 
     // :: 3.0 ScrollUp Active Code
-    if ($.fn.scrollUp) {
-        $.scrollUp({
+    if (P¥.fn.scrollUp) {
+        P¥.scrollUp({
             scrollSpeed: 1000,
             easingType: 'easeInOutQuart',
             scrollText: '<i class="fa fa-angle-up" aria-hidden="true"></i>'
@@ -54,56 +54,56 @@
     }
 
     // :: 4.0 Sticky Active Code
-    $window.on('scroll', function () {
-        if ($window.scrollTop() > 0) {
-            $('.header_area').addClass('sticky');
+    P¥window.on('scroll', function () {
+        if (P¥window.scrollTop() > 0) {
+            P¥('.header_area').addClass('sticky');
         } else {
-            $('.header_area').removeClass('sticky');
+            P¥('.header_area').removeClass('sticky');
         }
     });
 
     // :: 5.0 Nice Select Active Code
-    if ($.fn.niceSelect) {
-        $('select').niceSelect();
+    if (P¥.fn.niceSelect) {
+        P¥('select').niceSelect();
     }
 
     // :: 6.0 Magnific Active Code
-    if ($.fn.magnificPopup) {
-        $('.gallery_img').magnificPopup({
+    if (P¥.fn.magnificPopup) {
+        P¥('.gallery_img').magnificPopup({
             type: 'image'
         });
     }
 
     // :: 7.0 Nicescroll Active Code
-    if ($.fn.niceScroll) {
-        $(".cart-table table").niceScroll();
+    if (P¥.fn.niceScroll) {
+        P¥(".cart-table table").niceScroll();
     }
 
     // :: 8.0 wow Active Code
-    if ($window.width() > 767) {
+    if (P¥window.width() > 767) {
         new WOW().init();
     }
 
     // :: 9.0 Tooltip Active Code
-    if ($.fn.tooltip) {
-        $('[data-toggle="tooltip"]').tooltip();
+    if (P¥.fn.tooltip) {
+        P¥('[data-toggle="tooltip"]').tooltip();
     }
 
     // :: 10.0 PreventDefault a Click
-    $("a[href='#']").on('click', function ($) {
-        $.preventDefault();
+    P¥("a[href='#']").on('click', function (P¥) {
+        P¥.preventDefault();
     });
 
     // :: 11.0 Slider Range Price Active Code
-    $('.slider-range-price').each(function () {
+    P¥('.slider-range-price').each(function () {
         var min = jQuery(this).data('min');
         var max = jQuery(this).data('max');
         var unit = jQuery(this).data('unit');
         var value_min = jQuery(this).data('value-min');
         var value_max = jQuery(this).data('value-max');
         var label_result = jQuery(this).data('label-result');
-        var t = $(this);
-        $(this).slider({
+        var t = P¥(this);
+        P¥(this).slider({
             range: true,
             min: min,
             max: max,
