91574b9f8edbd800be2d8809ef5a4e3b2fcb5074
ayusou <samudiazayuso@gmail.com>
2024-12-02 17:02:02 +0100

    categorias

---
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/entities/categories/RegionCategory.java
@@ -0,0 +1,7 @@
+package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.categories;
+
+public class RegionCategory {
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/entities/categories/TypeCategory.java
@@ -0,0 +1,8 @@
+package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.categories;
+
+public class TypeCategory {
+    private Long id;
+    private String name;
+    private String description;
+
+}
