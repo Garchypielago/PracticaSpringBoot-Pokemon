2867ba1f4acd8eec770a5f6874e5dc2f448b5b9e
garchy-home <jandro.garcia.sanchez@gmail.com>
2024-12-02 16:35:47 +0100

    nuevas imaganes

--- shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/HomeController.java
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/HomeController.java
@@ -3,11 +3,6 @@ package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping({"/",""})
--- shop-Alejandro-Samuel/src/main/resources/static/js/active.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/active.js
@@ -1,14 +1,14 @@
-(function (P¥) {
+(function ($) {
     'use strict';
 
-    var P¥window = P¥(window);
+    var $window = $(window);
 
     // :: 1.0 Masonary Gallery Active Code
 
-    var proCata = P¥('.amado-pro-catagory');
+    var proCata = $('.amado-pro-catagory');
     var singleProCata = ".single-products-catagory";
 
-    if (P¥.fn.imagesLoaded) {
+    if ($.fn.imagesLoaded) {
         proCata.imagesLoaded(function () {
             proCata.isotope({
                 itemSelector: singleProCata,
@@ -21,32 +21,32 @@
     }
 
     // :: 2.1 Search Active Code
-    var amadoSearch = P¥('.search-nav');
-    var searchClose = P¥('.search-close');
+    var amadoSearch = $('.search-nav');
+    var searchClose = $('.search-close');
 
     amadoSearch.on('click', function () {
-        P¥('body').toggleClass('search-wrapper-on');
+        $('body').toggleClass('search-wrapper-on');
     });
 
     searchClose.on('click', function () {
-        P¥('body').removeClass('search-wrapper-on');
+        $('body').removeClass('search-wrapper-on');
     });
 
     // :: 2.2 Mobile Nav Active Code
-    var amadoMobNav = P¥('.amado-navbar-toggler');
-    var navClose = P¥('.nav-close');
+    var amadoMobNav = $('.amado-navbar-toggler');
+    var navClose = $('.nav-close');
 
     amadoMobNav.on('click', function () {
-        P¥('.header-area').toggleClass('bp-xs-on');
+        $('.header-area').toggleClass('bp-xs-on');
     });
 
     navClose.on('click', function () {
-        P¥('.header-area').removeClass('bp-xs-on');
+        $('.header-area').removeClass('bp-xs-on');
     });
 
     // :: 3.0 ScrollUp Active Code
-    if (P¥.fn.scrollUp) {
-        P¥.scrollUp({
+    if ($.fn.scrollUp) {
+        $.scrollUp({
             scrollSpeed: 1000,
             easingType: 'easeInOutQuart',
             scrollText: '<i class="fa fa-angle-up" aria-hidden="true"></i>'
@@ -54,56 +54,56 @@
     }
 
     // :: 4.0 Sticky Active Code
-    P¥window.on('scroll', function () {
-        if (P¥window.scrollTop() > 0) {
-            P¥('.header_area').addClass('sticky');
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 0) {
+            $('.header_area').addClass('sticky');
         } else {
-            P¥('.header_area').removeClass('sticky');
+            $('.header_area').removeClass('sticky');
         }
     });
 
     // :: 5.0 Nice Select Active Code
-    if (P¥.fn.niceSelect) {
-        P¥('select').niceSelect();
+    if ($.fn.niceSelect) {
+        $('select').niceSelect();
     }
 
     // :: 6.0 Magnific Active Code
-    if (P¥.fn.magnificPopup) {
-        P¥('.gallery_img').magnificPopup({
+    if ($.fn.magnificPopup) {
+        $('.gallery_img').magnificPopup({
             type: 'image'
         });
     }
 
     // :: 7.0 Nicescroll Active Code
-    if (P¥.fn.niceScroll) {
-        P¥(".cart-table table").niceScroll();
+    if ($.fn.niceScroll) {
+        $(".cart-table table").niceScroll();
     }
 
     // :: 8.0 wow Active Code
-    if (P¥window.width() > 767) {
+    if ($window.width() > 767) {
         new WOW().init();
     }
 
     // :: 9.0 Tooltip Active Code
-    if (P¥.fn.tooltip) {
-        P¥('[data-toggle="tooltip"]').tooltip();
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip();
     }
 
     // :: 10.0 PreventDefault a Click
-    P¥("a[href='#']").on('click', function (P¥) {
-        P¥.preventDefault();
+    $("a[href='#']").on('click', function ($) {
+        $.preventDefault();
     });
 
     // :: 11.0 Slider Range Price Active Code
-    P¥('.slider-range-price').each(function () {
+    $('.slider-range-price').each(function () {
         var min = jQuery(this).data('min');
         var max = jQuery(this).data('max');
         var unit = jQuery(this).data('unit');
         var value_min = jQuery(this).data('value-min');
         var value_max = jQuery(this).data('value-max');
         var label_result = jQuery(this).data('label-result');
-        var t = P¥(this);
-        P¥(this).slider({
+        var t = $(this);
+        $(this).slider({
             range: true,
             min: min,
             max: max,
