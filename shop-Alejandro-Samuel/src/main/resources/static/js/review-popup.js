b1a2cdbbfc7cadf6defcba2154aa5fcd1cb954a5
ayusou <samudiazayuso@gmail.com>
2025-01-15 12:44:07 +0100

    review stars sin terminar la parte de recoger los datos

--- shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
@@ -1,3 +1,5 @@
+//para que salga el pop up
+let canReview=true;
 const openBtn = document.getElementById("open-review");
 const closeBtn =document.getElementById("close-review");
 const review =document.getElementById("review")
@@ -10,4 +12,35 @@ openBtn.addEventListener("click",()=>{
 closeBtn.addEventListener("click",()=>{
     review.style.visibility="hidden";
     document.body.style.overflow = "auto";
+    canReview=false;
+    sendData();
 })
+
+//sistema de estrellas
+let stars = document.querySelectorAll(".rating-stars span")
+
+for(let star of stars){
+    star.addEventListener("click",function (){
+        let children=star.parentElement.children
+        for(let child of children){
+            if(child.getAttribute("data-clicked")){
+                if(canReview){
+                child.removeAttribute("data-clicked")}
+                else{
+                    return false
+                }
+            }
+        }
+
+        this.setAttribute("data-clicked","true")
+        let rating=this.dataset.rating;
+        let productId=this.dataset.productid;
+        console.log(rating,productId);
+    })
+}
+
+function sendData(){
+    let rating=this.dataset.rating;
+    let productId=this.dataset.productId;
+    console.log(rating,productId);
+}
