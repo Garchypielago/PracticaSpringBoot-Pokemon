91944e622639fc92ee52834a4209b5352082f1f1
Alejandro-Garchypielago <126523048+Garchypielago@users.noreply.github.com>
2025-01-15 11:37:03 +0100

    commit anda

--- shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
@@ -4,7 +4,10 @@ const review =document.getElementById("review")
 const reviewinner =document.getElementById("inner-review")
 openBtn.addEventListener("click",()=>{
     review.style.visibility="visible";
+    document.body.style.overflow = "hidden";
+
 })
 closeBtn.addEventListener("click",()=>{
     review.style.visibility="hidden";
+    document.body.style.overflow = "auto";
 })
