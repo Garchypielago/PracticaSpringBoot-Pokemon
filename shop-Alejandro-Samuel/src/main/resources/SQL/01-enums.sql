2b4c84c7efb61b69bf6f25408fbeb989f8fc6453
Alejandro-Garchypielago <126523048+Garchypielago@users.noreply.github.com>
2025-01-16 13:05:56 +0100

    sql pokemones

---
+++ shop-Alejandro-Samuel/src/main/resources/SQL/01-enums.sql
@@ -0,0 +1,38 @@
+INSERT INTO types (id, name, background)
+VALUES (1, 'NORMAL', ''),
+       (2, 'FIGHTING', ''),
+       (3, 'FLYING', ''),
+       (4, 'POISON', ''),
+       (5, 'GROUND', ''),
+       (6, 'ROCK', ''),
+       (7, 'BUG', ''),
+       (8, 'GHOST', ''),
+       (9, 'STEEL', ''),
+       (10, 'FIRE', '/shopedex/fondofuego.jpg'),
+       (11, 'WATER', '/shopedex/fondomar.jpg'),
+       (12, 'GRASS', '/shopedex/fondoarbol.jpg'),
+       (13, 'ELECTRIC', ''),
+       (14, 'PSYCHIC', ''),
+       (15, 'ICE', ''),
+       (16, 'DRAGON', ''),
+       (17, 'DARK', ''),
+       (18, 'FAIRY', '');
+
+INSERT INTO regions (id, name)
+VALUES (1, 'KANTO'),
+       (2, 'JOHTO'),
+       (3, 'HOENN'),
+       (4, 'SINNOH'),
+       (5, 'TESELIA'),
+       (6, 'KALOS'),
+       (7, 'ALOLA'),
+       (8, 'GALAR'),
+       (9, 'PALDEA');
+
+INSERT INTO stats (id, name)
+VALUES (1, 'HP'),
+       (2, 'Attack'),
+       (3, 'Defense'),
+       (4, 'Special Attack'),
+       (5, 'Special Defense'),
+       (6, 'Speed');
