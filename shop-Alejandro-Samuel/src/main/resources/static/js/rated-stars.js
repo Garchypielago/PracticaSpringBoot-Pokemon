2416f994c6b34d789eed893104c435873ea563c7
ayusou <samudiazayuso@gmail.com>
2025-01-23 18:43:10 +0100

    estrellas completadas

--- shop-Alejandro-Samuel/src/main/resources/static/js/rated-stars.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/rated-stars.js
@@ -1,3 +1,39 @@
-let stars = document.querySelectorAll(".ratings span")
-let rating
+let starrated = document.querySelectorAll(".ratings span")
+let rated = document.getElementById("avgRating").innerText
+if (rated<=0){
+
+}
+else if(rated<=0.5){
+    starrated[9].setAttribute("data-clicked","true")
+}
+else if(rated<=1){
+    starrated[8].setAttribute("data-clicked","true")
+}
+else if(rated<=1.5){
+    starrated[7].setAttribute("data-clicked","true")
+}
+else if(rated<=2){
+    starrated[6].setAttribute("data-clicked","true")
+}
+else if(rated<=2.5){
+    starrated[5].setAttribute("data-clicked","true")
+}
+else if(rated<=3){
+    starrated[4].setAttribute("data-clicked","true")
+}
+else if(rated<=3.5){
+    starrated[3].setAttribute("data-clicked","true")
+
+}else if(rated<=4){
+    starrated[2].setAttribute("data-clicked","true")
+
+}else if(rated<=4.5){
+    starrated[1].setAttribute("data-clicked","true")
+
+}
+else if(rated<=5){
+    starrated[0].setAttribute("data-clicked","true")
+
+}
+
 
