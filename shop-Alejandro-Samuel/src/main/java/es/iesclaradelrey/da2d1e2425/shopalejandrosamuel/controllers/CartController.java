0947a3b9a4faa076f6877f4f0012bc382bd4a5d4
ayusou <samudiazayuso@gmail.com>
2025-01-23 20:11:32 +0100

    va lo subo

--- shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/CartController.java
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/CartController.java
@@ -1,8 +1,28 @@
 package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.controllers;
 
+import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.ProductInCart;
+import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.repositories.ProductInCartRepository;
+import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.services.ProductInCartService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
 
 @Controller
+@RequestMapping({"/cart","/cart/"})
 public class CartController {
+    private final ProductInCartService productInCartService;
+    public CartController(ProductInCartService productInCartService){
+        this.productInCartService = productInCartService;
+    }
+ @GetMapping
+ public ModelAndView cartPage() {
+        Collection<ProductInCart> productInCarts = productInCartService.findAll();
+     ModelAndView mv = new ModelAndView("cart");
+     mv.addObject("productInCarts", productInCarts);
+     return mv;
 
+ }
 }
