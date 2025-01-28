fb552319a7aac2863feba01b20f7f7738fe07742
Alejandro-Garchypielago <126523048+Garchypielago@users.noreply.github.com>
2025-01-28 19:29:42 +0100

    reviews

--- shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/review-popup.js
@@ -1,6 +1,7 @@
 //para que salga el pop up
 let canReview=true;
 const openBtn = document.getElementById("open-review");
+const sendBtn =document.getElementById("send-review");
 const closeBtn =document.getElementById("close-review");
 const review =document.getElementById("review")
 const reviewtext =document.getElementById("review-area")
@@ -11,11 +12,15 @@ openBtn.addEventListener("click",()=>{
     document.body.style.overflow = "hidden";
 
 })
-closeBtn.addEventListener("click",()=>{
+sendBtn.addEventListener("click",()=>{
     review.style.visibility="hidden";
     document.body.style.overflow = "auto";
     canReview=false;
     sendData();
+})
+closeBtn.addEventListener("click",()=>{
+    review.style.visibility="hidden";
+    document.body.style.overflow = "auto";
 })
 
 //sistema de estrellas
