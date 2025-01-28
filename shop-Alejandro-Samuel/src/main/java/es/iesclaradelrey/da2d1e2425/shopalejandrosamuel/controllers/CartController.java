666a2d5567730c133c78afd3f00a827baacb92ad
ayusou <samudiazayuso@gmail.com>
2025-01-28 19:40:34 +0100

    cartbasecontroller

--- shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/CartController.java
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/controllers/CartController.java
@@ -2,6 +2,7 @@ package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.controllers;
 
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.ProductInCart;
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.repositories.ProductInCartRepository;
+import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.services.PokemonService;
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.services.ProductInCartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,18 +12,22 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Collection;
 
 @Controller
-@RequestMapping({"/cart","/cart/"})
-public class CartController {
+@RequestMapping({"/cart"})
+public class CartController extends BaseController {
+
     private final ProductInCartService productInCartService;
-    public CartController(ProductInCartService productInCartService){
+
+    public CartController(PokemonService pokemonService, ProductInCartService productInCartService) {
+        super(pokemonService);
         this.productInCartService = productInCartService;
     }
- @GetMapping
- public ModelAndView cartPage() {
-        Collection<ProductInCart> productInCarts = productInCartService.findAll();
-     ModelAndView mv = new ModelAndView("cart");
-     mv.addObject("productInCarts", productInCarts);
-     return mv;
 
- }
+    @GetMapping
+    public ModelAndView cartPage() {
+        Collection<ProductInCart> productInCarts = productInCartService.findAll();
+        ModelAndView mv = new ModelAndView("cart");
+        mv.addObject("products", productInCarts);
+        return mv;
+
+    }
 }
