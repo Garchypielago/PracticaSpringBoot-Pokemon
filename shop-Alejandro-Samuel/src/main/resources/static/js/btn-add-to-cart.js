62d4c0ff04b1e3b5a4ef521ef779d0eae1eaddf3
ayusou <samudiazayuso@gmail.com>
2025-02-18 20:01:09 +0100

    op

--- shop-Alejandro-Samuel/src/main/resources/static/js/btn-add-to-cart.js
+++ shop-Alejandro-Samuel/src/main/resources/static/js/btn-add-to-cart.js
@@ -1,13 +1,32 @@
+const errorCloseBtn = document.getElementById("errorCloseBtn");
+const error = document.getElementById("error")
+const errorDesc = document.getElementById("errorDesc")
+const errorTitle = document.getElementById("errorTitle")
+let currentStatus
+
+
+errorCloseBtn.addEventListener("click", () => {
+    error.style.visibility = "hidden";
+    document.body.style.overflow = "auto";
+})
+
+function openError(titleStatus, descriptionStatus) {
+    errorTitle.innerHTML = titleStatus
+    errorDesc.innerHTML = descriptionStatus
+    error.style.visibility = "visible";
+    document.body.style.overflow = "hidden";
+}
+
 
 const btnAddToCart = document.getElementById("btnAddToCart");
 const pokemonQuantity = document.getElementById("pokemonQuantity")
 
 // btnAddToCart.removeEventListener()
 btnAddToCart.addEventListener("click", () => {
-    fetch("/contextpath/api/v1/pokemones", {
+    fetch(appBasePath + "api/v1/pokemones", {
         method: "POST",
         body: JSON.stringify({
-            id: parseInt(btnAddToCart.getAttribute("data-pokemonid")) ,
+            id: parseInt(btnAddToCart.getAttribute("data-pokemonid")),
             productNumber: parseInt(pokemonQuantity.value)
             // id: btnAddToCart.getAttribute("data-pokemonId") ,
             // productNumber: pokemonQuantity.value
@@ -17,20 +36,20 @@ btnAddToCart.addEventListener("click", () => {
         }
     })
         .then(response => {
-
-            response.ok
-
-            // fetch("/contextpath/api/v1/pokemones")
-            //     .then(response => {
-            //         response.text().then(text => {
-            //                 const productos = JSON.parse(text);
-            //                 console.table(productos);
-            //             }
-            //         );
-            //
-            //     });
-            // console.log("ok")
-
+            if (response.status == 404 || response.status == 409){
+                throw response.status
+            }
         })
-        .catch(reason => alert("Error"))
-})
+        .then(data => {
+            //     toast
+        })
+        .catch(error => {
+            if (error === 409) {
+                openError("ERROR 409: Insufficient Stock", "You have requested too many pokemons of this type and we don't have enough stock");
+            } else if (error === 404) {
+                openError("ERROR 404: Pokemon not Found", "This pokemon does not exist");
+            } else {
+                openError("ERROR", "An unexpected error occurred");
+            }
+        });
+});
---
+++ shop-Alejandro-Samuel/src/main/resources/static/js/error-popup.js
@@ -0,0 +1,20 @@
+
+// const closeBtn =document.getElementById("errorCloseBtn");
+// const error =document.getElementById("error")
+// const desc =document.getElementById("errorDesc")
+// const title =document.getElementById("errorTitle")
+//
+//
+//
+// closeBtn.addEventListener("click",()=>{
+//     error.style.visibility="hidden";
+//     document.body.style.overflow = "auto";
+// })
+//
+// function openError(titleStatus,descriptionStatus){
+//     title.innerText=titleStatus
+//     desc.innerText=descriptionStatus
+//     error.style.visibility="visible";
+//     document.body.style.overflow = "hidden";
+//
+// }
