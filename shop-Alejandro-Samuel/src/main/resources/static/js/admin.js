28bb1b667b01b02b39807020cab8ed7886824713
Alejandro-Garchypielago <126523048+Garchypielago@users.noreply.github.com>
2025-02-20 18:26:22 +0100

    new

---
+++ shop-Alejandro-Samuel/src/main/resources/static/js/admin.js
@@ -0,0 +1,7 @@
+const selectTypeCategory = document.getElementById("select-type")
+const textBackground = document.getElementById("txt-background")
+const btnSendNewCategory = document.getElementById("btn-new-category")
+
+selectTypeCategory.addEventListener("change", ()=>{
+
+})
