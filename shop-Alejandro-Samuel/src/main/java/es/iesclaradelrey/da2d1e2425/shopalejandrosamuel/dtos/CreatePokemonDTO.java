09e6453de89e797ef69990522da1c6ee253e838b
ayusou <samudiazayuso@gmail.com>
2025-02-26 20:54:15 +0100

    le he hecho todo yo

--- shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/dtos/CreatePokemonDTO.java
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/dtos/CreatePokemonDTO.java
@@ -1,6 +1,9 @@
 package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.dtos;
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.Region;
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.entities.Type;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -9,14 +12,21 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class CreatePokemonDTO {
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @NotNull
     private Type type1;
     private Type type2;
+    @NotNull
     private Region region;
     private boolean legendary;
-
+    @NotNull
+    @Digits(integer = 10, fraction = 0)
     private Long stock;
+    @NotNull
+    @Digits(integer = 10, fraction = 0)
     private Long idReference;
 
 }
