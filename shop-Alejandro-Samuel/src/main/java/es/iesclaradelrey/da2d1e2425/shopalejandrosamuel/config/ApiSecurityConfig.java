0f3091cccb8f27f1e97ba2bd0a43ca3b5a09b18b
Alejandro-Garchypielago <126523048+Garchypielago@users.noreply.github.com>
2025-05-10 14:21:38 +0200

    Update ApiSecurityConfig.java

--- shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/config/ApiSecurityConfig.java
+++ shop-Alejandro-Samuel/src/main/java/es/iesclaradelrey/da2d1e2425/shopalejandrosamuel/config/ApiSecurityConfig.java
@@ -2,12 +2,15 @@ package es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.config;
 
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.filters.JwtAuthenticationFilter;
 import es.iesclaradelrey.da2d1e2425.shopalejandrosamuel.services.AppUserDetailsService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.*;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,30 +18,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 @Configuration
-@Order(3)
+@Order(1)
 public class ApiSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
-        http.securityMatcher("/api/**");
-
-        http.csrf(config -> config
-                .ignoringRequestMatchers("/api/**")
-        );
-
-        http.authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers("/api/app/**").authenticated()
-                                .anyRequest().permitAll()
-                );
-
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .securityMatcher("/api/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/app/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                        })
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-
-
-
-
 }
